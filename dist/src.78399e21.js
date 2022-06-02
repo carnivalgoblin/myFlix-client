e65e22008349e91919a1c6ab1b612954f9d3e3be
carnivalgoblin <rico.prodan@gmail.com>
2022-06-02 10:32:03 +0200

    fixed images

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -33575,17 +33575,17 @@ var MainView = /*#__PURE__*/function (_React$Component) {
         _id: 1,
         Title: 'Inception',
         Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-        ImagePath: '../images/inception.jpg'
+        ImagePath: './images/inception.jpg'
       }, {
         _id: 2,
         Title: 'The Shawshank Redemption',
         Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency. Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit.',
-        ImagePath: '../images/shawshank.jpg'
+        ImagePath: './images/shawshank.jpg'
       }, {
         _id: 3,
         Title: 'Gladiator',
         Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery. Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius.',
-        ImagePath: '../images/gladiator.jpg'
+        ImagePath: './images/gladiator.png'
       }],
       selectedMovie: null
     };
