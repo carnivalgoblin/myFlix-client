159b33031b46e26a5ee9894b65481beda4a8d1b4
carnivalgoblin <rico.prodan@gmail.com>
2022-06-03 11:40:08 +0200

    added typechecking

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
     const { movieData, onMovieClick } = this.props;
 
-    return <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
+    return (
+      <div className="movie-card" onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birth: PropTypes.number
+    }),
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string
+    }),
+    Actors: PropTypes.array,
+    Featured: PropTypes.bool,
+    _id: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick:PropTypes.func.isRequired
+};
