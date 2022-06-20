12a3c68153452fae39a9e8dae2bd067839aa967a
carnivalgoblin <rico.prodan@gmail.com>
2022-06-20 18:39:44 +0200

    added logout logic

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Button, Container } from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
+import PropTypes from 'prop-types';
+import Navbar from 'react-bootstrap/Navbar';
 
-export function Navbar() {
+export function Navbar(props) {
+
+  const onLogout = (e) => {
+    e.preventDefault();
+    props.onLoggedOut();
+  };
 
   return (
     <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
@@ -11,12 +18,16 @@ export function Navbar() {
 		  	  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 		  		  <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="ml-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#profile">Profile</Nav.Link>
-                <Nav.Link href="#logout">Logout</Nav.Link>
+                <Nav.Link href="#home" key='home'>Home</Nav.Link>
+                <Nav.Link href="#profile" key='profile'>Profile</Nav.Link>
+                <Nav.Link href="#logout" key='logout' onClick={onLogout}>Logout</Nav.Link>
               </Nav>
           </Navbar.Collapse>
 		  </Container>
 	  </Navbar>
   );
 };
+
+Navbar.propTypes = {
+    onLogout: PropTypes.func
+  };
